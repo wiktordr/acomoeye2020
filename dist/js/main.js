4089f57e65b8ab79f6c7b2d6c228e05e8cb309f7
Wiktor <wiktor.drozda@outlook.com>
2019-12-08 21:42:45 +0100

    initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,14 @@
+$('nav a').on('click', function(event){
+  if (this.hash !==''){
+    event.preventDefault();
+
+    const hash = this.hash;
+    
+    $('html, body').animate(
+      {scrollTop: $(hash).offset().top},
+      800
+    );
+
+    $('.toggler').prop('checked', false);
+  }
+});
